17a1530d1160e325b1e516039339d410ffb76782
Thomas Knive Karoliussen <thomas.karoliussen@rikstv.no>
2019-02-05 19:41:13 +0100

    File new project + "ship" and behaviour script

---
+++ Assets/Scripts/ShipBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipBehaviour : MonoBehaviour
+{
+    public float Speed = 5f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.JoystickButton0))
+            transform.position += transform.forward * Speed * Time.deltaTime;
+
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (Mathf.Abs(horizontal) > 0.1 || Mathf.Abs(vertical) > 0.1f)
+            Rotate();
+    }
+
+    private void Rotate(float horizontal, float vertical)
+    {
+    }
+}
